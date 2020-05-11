87fb1e325d7c3fae9620b0a58733d8bc82210e6f
Ayesh1998 <ayeshlak1998@gmail.com>
2020-05-11 13:59:33 +0530

    Added react spring animations to home top banner component name title

--- src/components/home-top-banner/home-top-banner-component.jsx
+++ src/components/home-top-banner/home-top-banner-component.jsx
@@ -1,16 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback, useRef, useState } from "react";
 import { Image, Jumbotron } from "react-bootstrap";
 import Aos from "aos";
+import { useSpring, animated, useTransition } from "react-spring";
 import "aos/dist/aos.css";
 
-const HomeTopBanner = (props) => {
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  0.9,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+const HomeTopBanner = (prop) => {
+  const [middleNameAnimation, setmiddleNameAnimation] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
+  //-------
+
+  const ref = useRef([]);
+  const [items, set] = useState([]);
+  const transitions = useTransition(items, null, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: "perspective(600px) rotateX(0deg)",
+      color: "#8fa5b6",
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: "perspective(600px) rotateX(180deg)", color: "#28d79f" },
+      { transform: "perspective(600px) rotateX(0deg)" },
+    ],
+    leave: [
+      { color: "#c23369" },
+      { innerHeight: 0 },
+      { opacity: 0, height: 0 },
+    ],
+    update: { color: "#28b4d7" },
+  });
+
+  const reset = useCallback(() => {
+    ref.current.map(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(
+      setTimeout(() => set(["Java", "JavaScript", "CSS"]), 2000)
+    );
+    ref.current.push(
+      setTimeout(() => set(["React", "React-Native", "Flutter"]), 5000)
+    );
+    ref.current.push(
+      setTimeout(() => set(["Dart", "Python", "Bootrap"]), 8000)
+    );
+    ref.current.push(
+      setTimeout(() => set(["Materialize", "MongoDB", "Firebase"]), 10000)
+    );
+  }, []);
+
+  //   useEffect(() => void reset(), []);
+
+  //----------
+
   return (
     <div className="home-top-banner">
-      <div className="home-top-banner-image" style={{ position: "relative" }}>
+      <div
+        className="home-top-banner-image"
+        style={{ position: "relative", cursor: "pointer" }}
+      >
         <img
           src={require("../../assets/black_background_fabric-wallpaper-2560x1440.jpg")}
           alt=""
@@ -79,23 +143,31 @@ const HomeTopBanner = (props) => {
             </p>
           </Jumbotron>
         </div>
-        <div
+        <animated.div
           className="home-banner-mydetailsMiddle"
           data-aos="fade-up"
           data-aos-duration="1000"
           data-aos-easing="ease"
           style={{
+            border: "3px white solid",
             position: "absolute",
             textAlign: "center",
-            top: "32%",
+            top: "45%",
             margin: "0 auto",
-            width: "100%",
+            width: "31%",
+            right: "30%",
+            transform: middleNameAnimation.xys.interpolate(trans),
           }}
+          onMouseMove={({ clientX: x, clientY: y }) =>
+            setmiddleNameAnimation({ xys: calc(x, y) })
+          }
+          onMouseLeave={() => setmiddleNameAnimation({ xys: [0, 0, 1] })}
         >
           <Jumbotron
             style={{
-              width: "67%",
-              margin: "0px auto",
+              width: "100%",
+              padding: "0px",
+              margin: "0px 0px",
               backgroundColor: "rgba(21, 19, 19, 0)",
               color: "white",
             }}
@@ -139,7 +211,21 @@ const HomeTopBanner = (props) => {
               </span>
             </div>
           </Jumbotron>
-        </div>
+        </animated.div>
+      </div>
+      <div>
+        {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+          <animated.div
+            className="transitions-item"
+            key={key}
+            style={rest}
+            onClick={reset}
+          >
+            <animated.div style={{ overflow: "hidden", height: innerHeight }}>
+              {item}
+            </animated.div>
+          </animated.div>
+        ))}
       </div>
     </div>
   );
