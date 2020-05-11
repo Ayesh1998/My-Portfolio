0f6299ab9eb2841af3bd435c1fc0cf553072a16f
Ayesh1998 <ayeshlak1998@gmail.com>
2020-05-12 00:44:50 +0530

    Added all the final data and finalize the skills-work-card-component

--- src/components/skills-work-cards/skills-work-cards-component.jsx
+++ src/components/skills-work-cards/skills-work-cards-component.jsx
@@ -9,21 +9,24 @@ const SkillsWorkCards = (prop) => {
   }, []);
 
   return (
-    <div className="container" style={{ marginTop: "5%" }}>
+    <div className="container" style={{ marginTop: "5%", marginBottom: "5%" }}>
       <CardDeck style={{ textAlign: "center" }}>
         <Card data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
           <Card.Img
             variant="top"
             src={require("../../assets/coding-skills.png")}
-            style={{ width: "63%", height: "40%", margin: "10px auto" }}
+            style={{ width: "63%", height: "45%", margin: "10px auto" }}
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>This is</Card.Text>
+            <Card.Title>Programming Skills</Card.Title>
+            <Card.Text>
+              JavaScript, Java, C, Python, ReactJS, React Native, Dart, Flutter,
+              etc....
+            </Card.Text>
           </Card.Body>
           <Card.Footer style={{ textAlign: "center" }}>
-            <Button variant="dark" style={{ margin: "0px auto" }}>
-              My Other Skills
+            <Button variant="dark" style={{ margin: "0px auto", width: "60%" }}>
+              Programming Skills
             </Button>
           </Card.Footer>
         </Card>
@@ -31,18 +34,17 @@ const SkillsWorkCards = (prop) => {
           <Card.Img
             variant="top"
             src={require("../../assets/my-work.png")}
-            style={{ width: "63%", height: "40%", margin: "10px auto" }}
+            style={{ width: "63%", height: "45%", margin: "10px auto" }}
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>My Work</Card.Title>
             <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
+              Mobile Applications, Web Applications, Desktop Apps, etc....{" "}
             </Card.Text>
           </Card.Body>
           <Card.Footer style={{ textAlign: "center" }}>
-            <Button variant="dark" style={{ margin: "0px auto" }}>
-              My Other Skills
+            <Button variant="dark" style={{ margin: "0px auto", width: "60%" }}>
+              My Work
             </Button>
           </Card.Footer>
         </Card>
@@ -50,15 +52,17 @@ const SkillsWorkCards = (prop) => {
           <Card.Img
             variant="top"
             src={require("../../assets/skills.png")}
-            style={{ width: "63%", height: "40%", margin: "10px auto" }}
+            style={{ width: "63%", height: "45%", margin: "10px auto" }}
           />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>This is a wide</Card.Text>
+            <Card.Title>Other Skills</Card.Title>
+            <Card.Text>
+              Time Managment, Data Structures & Algorithms, etc....
+            </Card.Text>
           </Card.Body>
           <Card.Footer style={{ textAlign: "center" }}>
-            <Button variant="dark" style={{ margin: "0px auto" }}>
-              My Other Skills
+            <Button variant="dark" style={{ margin: "0px auto", width: "60%" }}>
+              Other Skills
             </Button>
           </Card.Footer>
         </Card>
